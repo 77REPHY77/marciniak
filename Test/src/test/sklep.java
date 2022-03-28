bd880ea9f29b92cb3a9402cb1de0f47430b5aa82
77REPHY77 <didi.m88888@gmail.com>
2022-03-28 10:04:35 +0200

    .

--- Test/src/test/sklep.java
+++ Test/src/test/sklep.java
@@ -1,11 +1,29 @@
 package test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
 public class sklep {
-public static void main(String[] ags){
+public static void main(String[] ags) throws FileNotFoundException {
+
+			
 	
-	
+			
+			File plik = new File("Sklep");		
+			Scanner sc = new Scanner(plik);
+			int suma= 0;
+			while (sc.hasNextLine()) {
+		System.out.println(sc.next()+" "+sc.nextInt()+sc.nextInt());	
+		suma += sc.nextInt() * sc.nextInt();
+			
+			
+			
+
 	
 	
 	
 }
-}
+}}
